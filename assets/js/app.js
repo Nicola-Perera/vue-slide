d89298a4286667516de606ee183298225e083a2e
Nicola-Perera <nicola.perera96@mail.com>
2021-10-29 13:20:47 +0200

    added main picture section that show the picture with status on true and hides all status on false

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,27 +5,32 @@ const app = new Vue({
             {
                 title:'Svezia',
                 text:'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minima atque reiciendis unde deserunt, temporibus illum neque non sunt eum ducimus eligendi, ea odit fugiat quae? Fugiat nam odit accusantium maiores.',
-                image:'./img/01.jpg'
+                image:'./img/01.jpg',
+                status: false
             },
             {
                 title:'Svizzera',
                 text:'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minima atque reiciendis unde deserunt, temporibus illum neque non sunt eum ducimus eligendi, ea odit fugiat quae? Fugiat nam odit accusantium maiores.',
-                image:'./img/02.jpg'
+                image:'./img/02.jpg',
+                status: true
             },
             {
                 title:'Gran Bretagna',
                 text:'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minima atque reiciendis unde deserunt, temporibus illum neque non sunt eum ducimus eligendi, ea odit fugiat quae? Fugiat nam odit accusantium maiores.',
-                image:'./img/03.jpg'
+                image:'./img/03.jpg',
+                status: false
             },
             {
                 title:'Germania',
                 text:'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minima atque reiciendis unde deserunt, temporibus illum neque non sunt eum ducimus eligendi, ea odit fugiat quae? Fugiat nam odit accusantium maiores.',
-                image:'./img/04.jpg'
+                image:'./img/04.jpg',
+                status: false
             },
             {
                 title:'Paradise',
                 text:'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minima atque reiciendis unde deserunt, temporibus illum neque non sunt eum ducimus eligendi, ea odit fugiat quae? Fugiat nam odit accusantium maiores.',
-                image:'./img/05.jpg'
+                image:'./img/05.jpg',
+                status: false
             }
         ]
     }
