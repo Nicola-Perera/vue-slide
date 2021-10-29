d75e3a98c15b4d090255b5df2a75f0a3dbd93b38
Nicola-Perera <nicola.perera96@mail.com>
2021-10-29 13:05:37 +0200

    added countries' cards to markup by vue js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,32 @@
 const app = new Vue({
-    el: '#root'
+    el: '#root',
+    data: {
+        countries: [
+            {
+                title:'Svezia',
+                text:'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minima atque reiciendis unde deserunt, temporibus illum neque non sunt eum ducimus eligendi, ea odit fugiat quae? Fugiat nam odit accusantium maiores.',
+                image:'./img/01.jpg'
+            },
+            {
+                title:'Svizzera',
+                text:'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minima atque reiciendis unde deserunt, temporibus illum neque non sunt eum ducimus eligendi, ea odit fugiat quae? Fugiat nam odit accusantium maiores.',
+                image:'./img/02.jpg'
+            },
+            {
+                title:'Gran Bretagna',
+                text:'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minima atque reiciendis unde deserunt, temporibus illum neque non sunt eum ducimus eligendi, ea odit fugiat quae? Fugiat nam odit accusantium maiores.',
+                image:'./img/03.jpg'
+            },
+            {
+                title:'Germania',
+                text:'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minima atque reiciendis unde deserunt, temporibus illum neque non sunt eum ducimus eligendi, ea odit fugiat quae? Fugiat nam odit accusantium maiores.',
+                image:'./img/04.jpg'
+            },
+            {
+                title:'Paradise',
+                text:'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minima atque reiciendis unde deserunt, temporibus illum neque non sunt eum ducimus eligendi, ea odit fugiat quae? Fugiat nam odit accusantium maiores.',
+                image:'./img/05.jpg'
+            }
+        ]
+    }
 })
